64d5d02de36f04ef3494e8caa256388ccfeb5418
willian <will.oliver1994@hotmail.com>
2020-04-14 12:49:45 -0300

    completar código

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
 import Transaction from '../models/Transaction';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const initialBallance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    const balance = this.transactions.reduce((acc, transaction) => {
+      switch (transaction.type) {
+        case 'income':
+          return {
+            ...acc,
+            income: transaction.value + acc.income,
+            total: transaction.value + acc.income - acc.outcome,
+          };
+        case 'outcome':
+          return {
+            ...acc,
+            outcome: transaction.value + acc.outcome,
+            total: acc.income - transaction.value - acc.outcome,
+          };
+        default:
+          return acc;
+      }
+    }, initialBallance);
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
